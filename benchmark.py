be428690d8252c69b85b6e16e75e7a39ccb7991f
boris-il-forte <boris.ilpossente@hotmail.it>
2021-01-07 10:58:00 +0100

    Improved suite class
    - Added visualization methods
    - Refactored methods to add experiments:
    * Added add_environment to add a single environment
    * Added add_agent to add a single agent in an environment
    * Added add_experiments to add a list of algorithms for the same
    env

--- benchmark.py
+++ benchmark.py
@@ -3,14 +3,16 @@
 import yaml
 from pathlib import Path
 from argparse import ArgumentParser
-from mushroom_rl_benchmark import BenchmarkSuite, BenchmarkSuiteVisualizer
+from mushroom_rl_benchmark import BenchmarkSuite
+from mushroom_rl.core import Logger
 
 
 def get_args():
     parser = ArgumentParser()
     arg_test = parser.add_argument_group('benchmark parameters')
-    arg_test.add_argument("-e", "--env", type=str, required=True,
-                          help='Environment to benchmark.')
+    arg_test.add_argument("-e", "--env", type=str, nargs='+',
+                          help='Environments to be used by the benchmark. '
+                               'Use all, to select all environments available.')
     arg_test.add_argument("-s", "--slurm", action='store_true',
                           help='Flag to use of SLURM.')
     arg_test.add_argument("-q", "--sequential", action='store_true',
@@ -25,57 +27,72 @@ def get_args():
 
 
 if __name__ == '__main__':
-    env_id, use_slurm, sequential, test, demo = get_args()
+    env_ids, use_slurm, sequential, test, demo = get_args()
     cfg_dir = Path(__file__).parent / 'cfg'
-    config_file = cfg_dir / 'env' / (env_id + '.yaml')
+    env_cfg_dir = cfg_dir / 'env'
 
-    assert not (use_slurm and sequential)
+    logger = Logger(results_dir=None)
 
-    yaml_file = yaml.safe_load(open(config_file, 'r'))
-    run_params = yaml_file['run_params']
-    env_data = yaml_file['env_params']
-    agent_data = yaml_file['agent_params']
+    logger.info('Starting benchmarking script')
 
-    agents = agent_data.keys()
-    agents_params = agent_data.values()
-
-    env = env_data['name']
-    env_params = env_data['params']
-
-    print('Environment:', env)
-    print('Agents:', str(list(agents)))
+    if 'all' in env_ids:
+        logger.info('Running benchmark on all available environments')
+        assert len(env_ids) == 1
+        env_ids = list()
+        for env_id in env_cfg_dir.iterdir():
+            if env_id.suffix == '.yaml':
+                env_ids.append(env_id.stem)
 
     type_msg = 'parallel'
     type_msg = 'slurm' if use_slurm else type_msg
     type_msg = 'sequential' if sequential else type_msg
-    print('Execution type:', type_msg)
-    print('Runing FULL:', not demo)
-    print()
-
-    if demo:
-        run_params['n_runs'] = 4
-        run_params['n_epochs'] = 10
-        run_params['n_steps'] = 15000
-        run_params['n_episodes_test'] = 5
+    logger.info('Execution type:' + type_msg)
+    logger.info('Runing FULL:' +str(not demo))
+    logger.strong_line()
 
     exec_type = 'slurm' if use_slurm else 'parallel'
     exec_type = 'sequential' if sequential else 'parallel'
     param_file = 'params_slurm.yaml' if use_slurm else 'params_local.yaml'
 
     suite_params = yaml.safe_load(open(cfg_dir / param_file, 'r'))['suite_params']
-    
-    suite = BenchmarkSuite(
-        **suite_params,
-        **run_params)
 
-    for agent, agent_params in zip(agents, agents_params):
-        suite.add_experiment(env, env_params, agent, agent_params)
+    suite = BenchmarkSuite(**suite_params)
+
+    for env_id in env_ids:
+        config_file = cfg_dir / 'env' / (env_id + '.yaml')
+
+        assert not (use_slurm and sequential)
+
+        yaml_file = yaml.safe_load(open(config_file, 'r'))
+        run_params = yaml_file['run_params']
+        env_data = yaml_file['env_params']
+        agent_data = yaml_file['agent_params']
+
+        agents = agent_data.keys()
+        agents_params = agent_data.values()
+
+        env = env_data['name']
+        env_params = env_data['params']
+
+        logger.info('Adding environment:' + env)
+        logger.info('Adding agents:' + str(list(agents)))
+
+        if demo:
+            run_params['n_runs'] = 4
+            run_params['n_epochs'] = 10
+            run_params['n_steps'] = 15000
+            run_params['n_episodes_test'] = 5
+
+        suite.add_experiments(env, env_params, agents, agents_params, **run_params)
 
     suite.print_experiments()
+    logger.strong_line()
 
     if not test:
+        logger.info('Running the benchmarks')
+        logger.weak_line()
         suite.run(exec_type=exec_type)
 
-    if exec_type != 'slurm':
-        visualizer = BenchmarkSuiteVisualizer(suite.logger)
-        visualizer.save_reports()
+        if exec_type != 'slurm':
+            logger.info('Saving the plots on disk')
+            suite.save_plots()
--- mushroom_rl_benchmark/core/suite.py
+++ mushroom_rl_benchmark/core/suite.py
@@ -2,6 +2,7 @@ import mushroom_rl_benchmark.builders
 from mushroom_rl_benchmark.builders import EnvironmentBuilder
 from mushroom_rl_benchmark.core.experiment import BenchmarkExperiment
 from mushroom_rl_benchmark.core.logger import BenchmarkLogger
+from mushroom_rl_benchmark.core.visualizer import BenchmarkSuiteVisualizer
 
 
 class BenchmarkSuite:
@@ -9,45 +10,80 @@ class BenchmarkSuite:
     Class to orchestrate the execution of multiple experiments.
 
     """
-    def __init__(self, log_dir=None, log_id=None, use_timestamp=True, **run_params):
+    def __init__(self, log_dir=None, log_id=None, use_timestamp=True, **suite_params):
         """
         Constructor.
 
         Args:
             log_dir (str): path to the log directory (Default: ./logs or /work/scratch/$USER)
-            log_id (str): log id (Default: benchmark[_YY-mm-ddTHH:MM:SS.zzz])
+            log_id (str): log id (Default: benchmark[_YYYY-mm-dd-HH-MM-SS])
             use_timestamp (bool): select if a timestamp should be appended to the log id
-            **run_params: parameters that are passed to the run method of the experiment
+            **suite_params: parameters that are passed to the run method of the experiment
         
         """
-        self.experiment_structure = dict()
-        self.environment_list = []
-        self.agent_list = []
-        self.run_params = run_params
+        self._experiment_structure = dict()
+        self._environment_dict = dict()
+        self._agent_list = []
+        self._suite_params = suite_params
         self.logger = BenchmarkLogger(log_dir=log_dir, log_id=log_id, use_timestamp=use_timestamp)
 
-    def add_experiment(self, environment_name, environment_builder_params, agent_name, agent_builder_params):
+    def add_experiments(self, environment_name, environment_builder_params, agent_names_list,
+                        agent_builders_params, **run_params):
         """
-        Add an experiment to the suite.
+        Add a set of experiments for the same environment to the suite.
 
         Args:
-            environment_name (str): name of the environment for the experiment (E.g. Gym.Pendulum-v0)
-            environment_builder_params (dict): parameters for the environment builder
-            agent_name (str): name of the agent for the experiment
-            agent_builder_params (dict): parameters for the agent builder
+            environment_name (str): name of the environment for the experiment (E.g. Gym.Pendulum-v0);
+            environment_builder_params (dict): parameters for the environment builder;
+            agent_names_list (list): list of names of the agents for the experiments;
+            agent_builders_params (list): list of dictionaries containing the parameters for the agent builder;
+            run_params: Parameters that are passed to the run method of the experiment.
 
         """
-        if environment_name in self.environment_list:
-            if agent_name in self.experiment_structure[environment_name]:
-                raise AttributeError('An experiment for environment {} and builders {} already exists.'.format(environment_name, agent_name))
-            else:
-                self.experiment_structure[environment_name][agent_name] = self._create_experiment(environment_name, environment_builder_params, agent_name, agent_builder_params)
-        else:
-            self.environment_list.append(environment_name)
-            self.experiment_structure[environment_name] = {agent_name: self._create_experiment(environment_name, environment_builder_params, agent_name, agent_builder_params)}
-            
-        if agent_name not in self.agent_list:
-            self.agent_list.append(agent_name)
+        self.add_environment(environment_name, environment_builder_params, **run_params)
+
+        for agent_name, agent_params in zip(agent_names_list, agent_builders_params):
+            self.add_agent(environment_name, agent_name, agent_params)
+
+    def add_environment(self, environment_name, environment_builder_params, **run_params):
+        """
+        Add an environment to the benchmarking suite.
+
+        Args:
+            environment_name (str): name of the environment for the experiment (E.g. Gym.Pendulum-v0);
+            environment_builder_params (dict): parameters for the environment builder;
+            run_params: Parameters that are passed to the run method of the experiment.
+
+        """
+        if environment_name in self._environment_dict:
+            raise AttributeError('The environment {} has been already added to the benchmark'.format(environment_name))
+
+        self._environment_dict[environment_name] = dict(
+            build_params=environment_builder_params,
+            run_params=run_params
+        )
+
+        self._experiment_structure[environment_name] = dict()
+
+    def add_agent(self, environment_name, agent_name, agent_params):
+        """
+        Add an agent to the benchmarking suite.
+
+        Args:
+            environment_name (str): name of the environment for the experiment (E.g. Gym.Pendulum-v0);
+            agent_name (str): name of the agent for the experiments;
+            agent_params (list): dictionary containing the parameters for the agent builder.
+
+        """
+        assert environment_name in self._environment_dict
+        if agent_name in self._experiment_structure[environment_name]:
+            raise AttributeError(
+                'An experiment for environment {} and builders {} already exists.'.format(environment_name, agent_name)
+            )
+        self._agent_list.append(agent_name)
+        environment_builder_params = self._environment_dict[environment_name]['build_params']
+        self._experiment_structure[environment_name][agent_name] = \
+            self._create_experiment(environment_name, environment_builder_params, agent_name, agent_params)
 
     def _create_experiment(self, environment, environment_params, agent_name, agent_builder_params):
         separator = '.'
@@ -85,7 +121,7 @@ class BenchmarkSuite:
         Print the experiments in the suite.
 
         """
-        for env, agents in self.experiment_structure.items():
+        for env, agents in self._experiment_structure.items():
             for agent, _ in agents.items():
                 self.logger.info('Environment: {}\tAgent: {}'.format(env, agent))
 
@@ -94,7 +130,24 @@ class BenchmarkSuite:
         Run all experiments in the suite.
 
         """
-        for environment, agents in self.experiment_structure.items():
+        for environment, agents in self._experiment_structure.items():
             for agent, exp in agents.items():
                 self.logger.info('Starting Experiment for {} on {}'.format(agent, environment))
-                exp.run(exec_type=exec_type, **self.run_params)
+                run_params = self._environment_dict[environment]['run_params']
+                exp.run(exec_type=exec_type, **self._suite_params, **run_params)
+
+    def save_plots(self):
+        """
+        Save the result plot to the log directory.
+
+        """
+        visualizer = BenchmarkSuiteVisualizer(self.logger)
+        visualizer.save_reports()
+
+    def show_plots(self):
+        """
+        Display the result plot.
+
+        """
+        visualizer = BenchmarkSuiteVisualizer(self.logger)
+        visualizer.show_report()
