3574b93c60bf78eeb4a76f9a6f65bc07b0c2bafb
Boris_il_forte <boris.ilpossente@hotmail.it>
2021-02-20 18:40:00 +0100

    Updated PPO parameters

--- mushroom_rl_benchmark/builders/actor_critic/ppo.py
+++ mushroom_rl_benchmark/builders/actor_critic/ppo.py
@@ -49,8 +49,9 @@ class PPOBuilder(AgentBuilder):
         return agent._V(states).mean()
     
     @classmethod
-    def default(cls, actor_lr=3e-4, critic_lr=3e-4, critic_fit_params=None, critic_network=Network, lam=.95,
-                n_features=32, n_steps_per_fit=3000, preprocessors=None, use_cuda=False, get_default_dict=False):
+    def default(cls, eps=0.2, n_epochs_policy=4, actor_lr=3e-4, critic_lr=3e-4, critic_fit_params=None,
+                critic_network=Network, lam=.95, batch_size=64, n_features=32, n_steps_per_fit=3000,
+                preprocessors=None, use_cuda=False, get_default_dict=False):
         defaults = locals()
         
         policy_params = dict(
@@ -69,18 +70,18 @@ class PPOBuilder(AgentBuilder):
                 'params': {'lr': critic_lr}},
             loss=F.mse_loss,
             n_features=n_features,
-            batch_size=64,
+            batch_size=batch_size,
             output_shape=(1,))
 
         alg_params = dict(
             n_epochs_policy=4,
-            batch_size=64,
-            eps_ppo=.2,
+            batch_size=batch_size,
+            eps_ppo=eps,
             lam=lam,
             critic_fit_params=critic_fit_params)
 
         builder = cls(policy_params, actor_optimizer, critic_params, alg_params,
-                   n_steps_per_fit=n_steps_per_fit, preprocessors=preprocessors)
+                      n_steps_per_fit=n_steps_per_fit, preprocessors=preprocessors)
 
         if get_default_dict:
             return builder, defaults
