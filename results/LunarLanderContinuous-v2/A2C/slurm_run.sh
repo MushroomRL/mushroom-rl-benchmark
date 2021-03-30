d5683bc7bf51a958d8d16525917f4f7445a98b25
boris-il-forte <boris.ilpossente@hotmail.it>
2021-03-30 11:03:12 +0200

    Updated results for lunar lander and pendulum
    - using new TD3 exploration policy

--- results/LunarLanderContinuous-v2/A2C/slurm_run.sh
+++ results/LunarLanderContinuous-v2/A2C/slurm_run.sh
@@ -8,14 +8,14 @@
 #SBATCH -n 1
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=4000
-#SBATCH -o /home/tateo/mushroom-rl-benchmark/logs/benchmark/LunarLanderContinuous-v2/A2C/%A_%a.out
-#SBATCH -e /home/tateo/mushroom-rl-benchmark/logs/benchmark/LunarLanderContinuous-v2/A2C/%A_%a.err
+#SBATCH -o /work/scratch/dt11kypo/logs/benchmark/LunarLanderContinuous-v2/A2C/%A_%a.out
+#SBATCH -e /work/scratch/dt11kypo/logs/benchmark/LunarLanderContinuous-v2/A2C/%A_%a.err
 ###############################################################################
 # Your PROGRAM call starts here
 echo "Starting Job $SLURM_JOB_ID, Index $SLURM_ARRAY_TASK_ID"
 
 # Program specific arguments
-CMD="python3 /home/tateo/mushroom-rl-benchmark/mushroom_rl_benchmark/experiment/slurm/run_script.py \
+CMD="python3 /work/home/dt11kypo/mushroom-rl-benchmark/mushroom_rl_benchmark/experiment/slurm/run_script.py \
 		${@:1}\
 		--seed $SLURM_ARRAY_TASK_ID"
 
---
+++ results/Pendulum-v0/TRPO/slurm_aggregate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# SLURM Configurations
+#SBATCH -J Pendulum-v0/TRPO_aggregate
+#SBATCH -t 03:00:00
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=2000
+#SBATCH -o /work/scratch/dt11kypo/logs/benchmark/Pendulum-v0/TRPO/%A.out
+#SBATCH -e /work/scratch/dt11kypo/logs/benchmark/Pendulum-v0/TRPO/%A.err
+###############################################################################
+# Your PROGRAM call starts here
+echo "Starting Job $SLURM_JOB_ID, Index $SLURM_ARRAY_TASK_ID"
+
+# Program specific arguments
+CMD="python3 /work/home/dt11kypo/mushroom-rl-benchmark/mushroom_rl_benchmark/experiment/slurm/aggregate_results.py \
+		${@:1}\
+		--seed 0"
+
+echo "$CMD"
+eval $CMD
