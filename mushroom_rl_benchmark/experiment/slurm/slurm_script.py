360cafcb3bca2ea52c1b38592f27b6e7e4886a69
boris-il-forte <boris.ilpossente@hotmail.it>
2021-06-15 11:07:04 +0200

    Slurm code cleanup + support for partition
    - now you can specify a partition to use with slurm
    - slurm code cleanup

--- mushroom_rl_benchmark/experiment/slurm/slurm_script.py
+++ mushroom_rl_benchmark/experiment/slurm/slurm_script.py
@@ -26,7 +26,7 @@ def create_slurm_script(slurm_path, slurm_script_name='slurm.sh', **slurm_params
     return slurm_path
 
 
-def generate_slurm(exp_name, exp_dir_slurm, python_file, gres=None, project_name=None, n_exp=1,
+def generate_slurm(exp_name, exp_dir_slurm, python_file, gres=None, project_name=None, partition=None, n_exp=1,
                    max_concurrent_runs=None, memory=2000, hours=24, minutes=0, seconds=0):
     """
     Function to generate the slurm file content.
@@ -37,6 +37,7 @@ def generate_slurm(exp_name, exp_dir_slurm, python_file, gres=None, project_name
         python_file (str): path to the python file that should be executed;
         gres (str, None): request cluster resources. E.g. to add a GPU in the IAS cluster specify gres='gpu:rtx2080:1';
         project_name (str, None): name of the slurm project;
+        partition (str, None): name of the partition to be used.
         n_exp (int, 1): number of experiments in the slurm array;
         max_concurrent_runs (int, None): maximum number of runs that should be executed
             in parallel on the SLURM cluster;
@@ -49,50 +50,62 @@ def generate_slurm(exp_name, exp_dir_slurm, python_file, gres=None, project_name
         The slurm script as string.
         
     """
-    duration = to_duration(hours, minutes, seconds) 
-    code = """\
+    duration = to_duration(hours, minutes, seconds)
+
+    project_name_option = ''
+    partition_option = ''
+    gres_option = ''
+    job_array_option = ''
+
+    if project_name:
+        project_name_option = f'#SBATCH -A {project_name}\n'
+    if partition:
+        partition_option = f'#SBATCH -p {partition}\n'
+    if gres:
+        gres_option = f'#SBATCH --gres={gres}\n'
+
+
+    if n_exp > 1:
+        job_array_option += '#SBATCH -a 0-' + str(n_exp - 1) + (
+            '%{}'.format(max_concurrent_runs) if max_concurrent_runs is not None else '') + '\n'
+
+        text_output_file = '#SBATCH -o ' + exp_dir_slurm + '/%A_%a.out\n'
+        text_output_file += '#SBATCH -e ' + exp_dir_slurm + '/%A_%a.err\n'
+        seed_specification = '\t\t--seed $SLURM_ARRAY_TASK_ID\n'
+    else:
+        text_output_file = '#SBATCH -o ' + exp_dir_slurm + '/%A.out\n'
+        text_output_file += '#SBATCH -e ' + exp_dir_slurm + '/%A.err\n'
+        seed_specification = '\t\t--seed 0'
+
+    code = f"""\
 #!/usr/bin/env bash
 
 ###############################################################################
 # SLURM Configurations
-"""
-    if project_name:
-        code += '#SBATCH -A ' + project_name + '\n'
-    code += '#SBATCH -J ' + exp_name + '\n'
-    if n_exp > 1:
-        code += '#SBATCH -a 0-' + str(n_exp-1) + ('%{}'.format(max_concurrent_runs) if max_concurrent_runs is not None else '') + '\n'
-    code += '#SBATCH -t ' + duration + '\n'
-    code += """\
+
+# Optional parameters
+{project_name_option}{partition_option}{gres_option}
+# Mandatory parameters
+#SBATCH -J {exp_name}
+{job_array_option}#SBATCH -t {duration}
 #SBATCH -n 1
 #SBATCH -c 1
-"""
-    code += '#SBATCH --mem-per-cpu=' + str(memory) + '\n'
-    if gres:
-        code += '#SBATCH --gres=' + str(gres) + '\n'
-    if n_exp > 1:
-        code += '#SBATCH -o ' + exp_dir_slurm + '/%A_%a.out\n'
-        code += '#SBATCH -e ' + exp_dir_slurm + '/%A_%a.err\n'
-    else:
-        code += '#SBATCH -o ' + exp_dir_slurm + '/%A.out\n'
-        code += '#SBATCH -e ' + exp_dir_slurm + '/%A.err\n'
-    code += """\
+#SBATCH --mem-per-cpu={memory}
+{text_output_file}
 ###############################################################################
 # Your PROGRAM call starts here
 echo "Starting Job $SLURM_JOB_ID, Index $SLURM_ARRAY_TASK_ID"
 
 # Program specific arguments
-"""
-    code += 'CMD="python3 ' + python_file + ' \\\n'
-    code += "\t\t${@:1}\\\n"
+CMD="python3 {python_file} \\
+\t\t${{@:1}}\\
+{seed_specification}
 
-    if n_exp > 1:
-        code += '\t\t--seed $SLURM_ARRAY_TASK_ID'
-    else:
-        code += '\t\t--seed 0'
-    
-    code += '"\n\n'
-    code += 'echo "$CMD"\n'
-    code += 'eval $CMD\n'
+echo \"$CMD\"
+eval $CMD
+
+
+"""
 
     return code
 
